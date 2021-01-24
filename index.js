cc166748a3b7c7926c622a13023f2c5aab7bf090
Jessica Guan <jesy@Jessicas-MacBook-Pro.local>
2021-01-23 22:35:03 -0500

    added basic design elements to pages and some basic function. No calls to backend implemented

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+//This function should eventually call the api for a match and then reload the page
+//with a new conversation with the matched person
 function match() {
-    document.getElementById("text").innerHTML = "Paragraph changed.";
+    document.getElementById("text").innerHTML = "No one to talk to...";
+}
+
+//This function should eventually get the data for the current page and then display
+//the proper list of conversations and the proper open conversation
+function loadConversations() {
+    document.getElementById("convo").innerHTML += "<p> Jane Doe </p>"
+    document.getElementById("text").innerHTML += "<br> Talk to me please!";
 }
